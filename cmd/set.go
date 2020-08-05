969dd2816024b570ace408c95df278ea6c4c3384
Kareem Daggash <kareemdaggash@gmail.com>
2020-08-05 22:15:12 +0100

    docs: add comments to timer set cmd

--- cmd/set.go
+++ cmd/set.go
@@ -11,10 +11,12 @@ import (
   "github.com/spf13/cobra"
 )
 
+// initialise amd add command to rootCmd
 func init() {
   rootCmd.AddCommand(setCmd)
 }
 
+// initialise command vars and timer duration
 var (
 	timer time.Duration = 0 
 	setCmd = &cobra.Command{
@@ -27,12 +29,16 @@ var (
 	}
 )
 
+// parses to ensure there is only 1 argument passed in
+// and that it is a valid time duration 
 func parseArgs(cmd *cobra.Command, args []string) error {
+	// only 1 arg
 	err := cobra.ExactArgs(1)(cmd, args)
 	if err != nil {
 		return err
 	}
 
+	// arg is valid time duration
 	value := strings.Join(args, " ")
 	timer, err = time.ParseDuration(value)
 	if err != nil {
@@ -43,12 +49,15 @@ func parseArgs(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
+// setTimer starts a timer for a set amount of time as a goroutine
+// notifications are sent at the start and end of the timer
 func setTimer(cmd *cobra.Command, args []string) {
 	title := "⏰ Timer CLI - Countdown Started"
 	body := fmt.Sprintf("A timer has been started for %s", timer)
 	icon := "assets/information.png"
 	beeep.Notify(title, body, icon)
 	
+	// start timer in a waitgroup in case multiple timers started simultaneously 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
@@ -59,11 +68,12 @@ func setTimer(cmd *cobra.Command, args []string) {
 		done := time.Until(time.Now().Add(timer))
 		<-time.After(done)
 
-		// update title and body
+		// update title and body and notify
 		title = "⏰ Timer CLI - Countdown finished"
 		body = fmt.Sprintf("Your timer of %v is over.", args[0])
 		beeep.Notify(title, body, icon)
 	}()
 
+	// wait for all the goroutines launched here to finish.
 	wg.Wait()
 }
